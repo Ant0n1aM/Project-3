e60dbb14638a745e7b75e28481167bef17055688
Ant0n1aM <tmarisca32@gmail.com>
2025-12-02 06:13:29 +0000

    Fitting the parabola to the users inputs and reporting the output

--- proj3_1.m
+++ proj3_1.m
@@ -2,3 +2,12 @@
 % Ask the user for three sets of (x,y) 
 x = input('Enter x values as a vector [x1 x2 x3]: ');
 y = input('Enter y values as a vector [y1 y2 y3]: ');
+
+% Fitting the parabola 
+p = polyfit(x, y, 2);
+
+% Reporting the output
+fprintf("The solution to that system is " + ...
+    "a = %.f, b = %.f, c = %.f.",p(1),p(2),p(3));
+fprintf("\nSo, the equation for the parabola is, " + ...
+    "y = %.fx^2 + %.fx + %.f\n",p(1),p(2),p(3));
