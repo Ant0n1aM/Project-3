b99c751eb76136830c994e906ff10ea3c751e17e
Ant0n1aM <tmarisca32@gmail.com>
2025-12-02 19:51:07 +0000

    Created a  for loop to ask for the information and create each object of the player class.

--- proj3_4.m
+++ proj3_4.m
@@ -1,3 +1,13 @@
 % Calculating the average & on base percentage for each player
 % Define user inputs 
 numPlayers = input("How many players do you want to enter? ");
+
+% Use a for loop to ask for the information and create each object
+% of the player class
+for i = 1:numPlayers
+    name = input("Enter the name of the baseball player: ",'s');
+    team = input("Enter the name of the team they play on: ");
+    hits = input("Enter their number of hits: ");
+    atBats = input("Enter their number of at bats: ");
+    walks = input("Enter their number of walks: ");
+end
