458107b92fde2e45a6c16725f10065e5add20fc0
Ant0n1aM <tmarisca32@gmail.com>
2025-12-02 06:22:13 +0000

    Graphing the parabola and the three points

--- proj3_1.m
+++ proj3_1.m
@@ -11,3 +11,11 @@ fprintf("The solution to that system is " + ...
     "a = %.f, b = %.f, c = %.f.",p(1),p(2),p(3));
 fprintf("\nSo, the equation for the parabola is, " + ...
     "y = %.fx^2 + %.fx + %.f\n",p(1),p(2),p(3));
+
+% Graphing the parabola and the three points
+xlim = -10:0.1:10;
+plot(xlim, polyval(p,xlim),'green')
+hold on 
+plot(x,y,'ro','linewidth',2)
+grid on 
+title("Graph of Polynomial")
