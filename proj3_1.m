9f11229b12e85c5d675f2f5bb5e9fb81b3fec94a
Ant0n1aM <tmarisca32@gmail.com>
2025-12-02 06:01:41 +0000

    Asking  the user for three sets of (x,y) with them entering it as a vector [x1 x2 x3]

--- proj3_1.m
+++ proj3_1.m
@@ -1,1 +1,4 @@
 % System of Equations – Finding the Equation of a Parabola
+% Ask the user for three sets of (x,y) 
+x = input('Enter x values as a vector [x1 x2 x3]: ');
+y = input('Enter y values as a vector [y1 y2 y3]: ');
