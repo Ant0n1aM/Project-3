1e6b947b2b498bea66768966f8f432b97225e044
Erynn H. <ehpadhal1@my.scccd.edu>
2025-12-06 07:03:29 +0000

    Cleaned up script  to go off given exaple, made comments telling what each step does.

--- proj3_4.m
+++ proj3_4.m
@@ -1,31 +1,31 @@
 % Calculating the average & on base percentage for each player
 % Define user inputs 
-numPlayers = input("How many players do you want to enter?\n");
+numPlayers = input("How many players do you want to enter?:");
 
 % Use a for loop to ask for the information and create each object
 % of the player class
 for i = 1:numPlayers
-    name = input("\nEnter the name of the baseball player: ",'s');
+    name = input("Enter the name of the baseball player: ",'s');
     team = input("Enter the name of the team they play on: ",'s');
     hits = input("Enter their number of hits: ");
     atBats = input("Enter their number of at bats: ");
     walks = input("Enter their number of walks: ");
     players(i) = player(name, team, hits, walks, atBats);
 end
-% Determine and display players who had a batting average above 0.300
-fprintf('\n\n\nThe players with a batting average higher than 0.300 are:\n\n')
+%The output of the program
+% A list of all players, with their team name, with batting averages greater than 0.300
+fprintf('\nThe players with a batting average higher than 0.300 are:\n')
 for i = 1:numPlayers
     if players(i).average > 0.300
-        fprintf('%s:\nTeam: %s\nAverage:\t\t\t%.3f\nOn-Base Percentage:\t%.3f\n\n', ...
+        fprintf('\n%s:\nTeam:%s\nAverage:\t\t\t%.3f\nOn Base Percentage:\t%.3f\n\n', ...
             players(i).name,players(i).team,players(i).average,players(i).onBasepercent)
     end
 end
-% Determine and display players who had an on-base percentage higher than
-% 0.350
-fprintf('\n\n\nThe players with an on-base percentage higher than 0.350 are:\n\n')
+% A list of all players ,with their team name, with on-base percentages greater than 0.350
+fprintf('\nThe players with an on-base percentage higher than 0.350 are:\n')
 for i = 1:numPlayers
     if players(i).onBasepercent > 0.350
-        fprintf('%s:\nTeam: %s\nAverage:\t\t\t%.3f\nOn-Base Percentage:\t%.3f\n\n', ...
+        fprintf('\n%s:\nTeam:%s\nAverage:\t\t\t%.3f\nOn Base Percentage:\t%.3f\n', ...
             players(i).name,players(i).team,players(i).average,players(i).onBasepercent)
     end
 end
